3fbfb9ddf2b89e80502d83a2b367e431012629a0
Frank Mazzarella <mazzarella.frank@gmail.com>
2024-04-13 17:24:57 -0400

    connecting to backend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
+import { useEffect, useState } from 'react';
+
 import './App.css';
 
-// 🗘 ⊗
-
 function App() {
+  const UNICODE_UP = '↑';
+  const UNICODE_DOWN = '↓';
+  const UNICODE_LOADING = '🗘';
+  const UNICODE_ERROR = '⊗';
+
+  const [statusLeft, setStatusLeft] = useState(UNICODE_LOADING);
+  const [statusRight, setStatusRight] = useState(UNICODE_LOADING);
+
+  useEffect(() => {
+    async function queryStatus() {
+      try {
+        const response = await fetch('http://localhost:4000/api/v1/status');
+        const status = await response.json();
+        setStatusLeft(status.left === 'up' ? UNICODE_UP : UNICODE_DOWN);
+        setStatusRight(status.right === 'up' ? UNICODE_UP : UNICODE_DOWN);
+      } catch (error) {
+        console.error(error);
+        setStatusLeft(UNICODE_ERROR);
+        setStatusRight(UNICODE_ERROR);
+      }
+    }
+    queryStatus();
+  }, []);
+
   return (
     <div className="container">
       <div className="status">
-        <span>↑</span>
-        <span>↓</span>
+        <span>{ statusLeft }</span>
+        <span>{ statusRight }</span>
       </div>
     </div>
   );
