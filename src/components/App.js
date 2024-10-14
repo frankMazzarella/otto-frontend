782a355f29bb3964a55829efd12660f7d4b828d0
Frank Mazzarella <mazzarella.frank@gmail.com>
2024-10-14 14:26:07 -0400

    moving context out of index and into app

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
+import { GarageStatusContextProvider } from "../context/GarageStatusContext";
+import { ApiEndpointContextProvider } from "../context/ApiEndpointcontext";
 import { Environment } from "./Environment";
 import { Status } from "./Status";
 import { Buttons } from "./Buttons";
@@ -5,14 +7,17 @@ import "./App.css";
 
 const version = `v${process.env.REACT_APP_VERSION}`;
 
-// TODO: should not house entire application in App()
 export const App = () => {
   return (
-    <div className="app-container">
-      <Status />
-      <Environment />
-      <Buttons />
-      <div className="version">{version}</div>
-    </div>
+    <ApiEndpointContextProvider>
+      <GarageStatusContextProvider>
+        <div className="app-container">
+          <Status />
+          <Environment />
+          <Buttons />
+          <div className="version">{version}</div>
+        </div>
+      </GarageStatusContextProvider>
+    </ApiEndpointContextProvider>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,16 +3,10 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 import { App } from "./components/App";
-import { GarageStatusContextProvider } from "./context/GarageStatusContext";
-import { ApiEndpointContextProvider } from "./context/ApiEndpointcontext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ApiEndpointContextProvider>
-      <GarageStatusContextProvider>
-        <App />
-      </GarageStatusContextProvider>
-    </ApiEndpointContextProvider>
+    <App />
   </React.StrictMode>
 );
