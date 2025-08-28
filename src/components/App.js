db669869163498b0bb889afe252a9fba01c57f2d
Frank Mazzarella <mazzarella.frank@gmail.com>
2025-08-27 22:07:46 -0400

    shifting garage status provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 
-import { GarageStatusContextProvider } from "../context/GarageStatusContext";
 import { ApiEndpointContextProvider } from "../context/ApiEndpointcontext";
 import { Garage } from "./garage/Garage";
 import { Hvac } from "./hvac/Hvac";
@@ -11,17 +10,15 @@ const version = `v${process.env.REACT_APP_VERSION}`;
 export const App = () => {
   return (
     <ApiEndpointContextProvider>
-      <GarageStatusContextProvider>
-        <div className="app-container">
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Garage />} />
-              <Route path="/hvac" element={<Hvac />} />
-            </Routes>
-          </BrowserRouter>
-          <div className="version">{version}</div>
-        </div>
-      </GarageStatusContextProvider>
+      <div className="app-container">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Garage />} />
+            <Route path="/hvac" element={<Hvac />} />
+          </Routes>
+        </BrowserRouter>
+        <div className="version">{version}</div>
+      </div>
     </ApiEndpointContextProvider>
   );
 };
--- src/components/garage/Garage.js
+++ src/components/garage/Garage.js
@@ -1,13 +1,14 @@
+import { GarageStatusContextProvider } from "../../context/GarageStatusContext";
 import { Environment } from "./Environment";
 import { Status } from "./Status";
 import { Buttons } from "./Buttons";
 
 export const Garage = () => {
   return (
-    <>
+    <GarageStatusContextProvider>
       <Status />
       <Environment />
       <Buttons />
-    </>
+    </GarageStatusContextProvider>
   );
 };
